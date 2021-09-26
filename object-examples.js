2e603bda1a4775c2a921193bd13fd63166915319
Rohtia <90939780+Rohtia@users.noreply.github.com>
2021-09-26 13:05:37 -0400

    Animal object added

--- object-examples.js
+++ object-examples.js
@@ -1,7 +1,15 @@
-var Car = {
+var car = {
   make: 'Nissan',
   model: 'Versa Note',
   year: '2018',
   colour: 'black',
   doors: 5
+};
+
+
+var animal = {
+  name: 'cat',
+  type: 'mammal',
+  age: 3,
+  colour: 'black and white'
 };
